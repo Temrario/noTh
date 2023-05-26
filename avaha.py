3c9a29ddc04fff861c70ee4a4ffc868227e2cd4e
temrario <vaninmixa02@gmail.com>
2023-05-26 15:27:51 +0300

    star

--- avaha.py
+++ avaha.py
@@ -6,18 +6,37 @@ window.bgcolor("white")
 
 # Ініціалізація черепашки
 star = turtle.Turtle()
-star.color("blue")
+star.color("purple")
 star.speed(3)
 
-# Малюємо зірку
-star.begin_fill()
-for _ in range(5):
-    star.forward(100)
-    star.right(144)
-star.end_fill()
+# Функція для малювання зірки
+def draw_star():
+    star.begin_fill()
+    for _ in range(5):
+        star.forward(100)
+        star.right(144)
+    star.end_fill()
+
+# Малюємо першу зірку
+draw_star()
+
+# Переміщуємо черепашку для другої зірки
+star.penup()
+star.goto(200, 0)
+star.pendown()
+
+# Малюємо другу зірку
+draw_star()
+
+# Переміщуємо черепашку для третьої зірки
+star.penup()
+star.goto(-200, 0)
+star.pendown()
+
+# Малюємо третю зірку
+draw_star()
 
 # Закриваємо вікно при кліку на нього
 turtle.done()
-
 
 print("avaha said - HI!")
